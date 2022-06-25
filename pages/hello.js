096a3d508df1add9594f23fdb302cd4f4bde065a
babuamrit <92739498+babuamrit@users.noreply.github.com>
2022-06-25 18:20:29 +0545

    Revert "Revert "after animation""
    This reverts commit 6db9998c8b4a445ba02d042af8ff70cfe7d76fb5.

--- pages/hello.js
+++ pages/hello.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useAnimation, motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
-const hello = () => {
-  return <div>hello</div>;
+const squareVariants = {
+  visible: { opacity: 1, scale: 4, transition: { duration: 1 } },
+  hidden: { opacity: 0, scale: 0 }
 };
-
-export default hello;
+function Square() {
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
+  return (
+    <motion.div
+      ref={ref}
+      animate={controls}
+      initial="hidden"
+      variants={squareVariants}
+      className="square"
+    >   <a className="hp-fp-102 ">RoadMap Board</a></motion.div>
+  );
+}
+export default function App() {
+  return (
+    <div className="App">
+      <h1 className="title">Scroll Down</h1>
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+    </div>
+  );
+}
